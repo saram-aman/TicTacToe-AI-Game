b17a46167a019b4dd878bcf9bc6c84a5f18480c7
root <root@DESKTOP-CSPRQ1M>
2023-07-11 15:21:17 +0530

    work: initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './App.css'
+export default class App extends React.Component {
+    render() {
+        return (
+            <>
+                <div className="h-screen bg-blue-500 flex justify-center items-center">
+                    <h1 className="text-center text-3xl font-bold underline">Hello world!</h1>
+                </div>
+            </>
+        )
+    }
+}
